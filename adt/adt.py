4f338b74b55458971feeaa6f0e86a0b6eec83066
Agam Thakur <51986249+Agam17@users.noreply.github.com>
2021-11-14 10:15:34 -0500

    Update adt.py

--- adt/adt.py
+++ adt/adt.py
@@ -1,36 +1,3 @@
-#!/usr/bin/python3
-# encoding: UTF-8
-
-#    This file is part of IntelSpy
-#    Copyright (C) 2020 @maldevel
-#    https://github.com/maldevel/intelspy
-#
-#    IntelSpy - Perform automated network reconnaissance scans.
-#	 Gather network intelligence.
-#
-#    This program is free software: you can redistribute it and/or modify
-#    it under the terms of the GNU General Public License as published by
-#    the Free Software Foundation, either version 3 of the License, or
-#    (at your option) any later version.
-#
-#    This program is distributed in the hope that it will be useful,
-#    but WITHOUT ANY WARRANTY; without even the implied warranty of
-#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-#    GNU General Public License for more details.
-#
-#    You should have received a copy of the GNU General Public License
-#    along with this program.  If not, see <http://www.gnu.org/licenses/>.
-#
-#    For more see the file 'LICENSE' for copying permission.
-
-
-# Created by @maldevel | Logisek ICT
-# https://logisek.com | https://pentest-labs.com
-# intelspy.py | Python3
-# Released under GPL Version 3 License
-# 2020
-
-
 import atexit
 import argparse
 import asyncio
